32bfaccfa7ab255451a43793f4859f902407ffb7
sakib <nsakib.cse@gmail.com>
2021-06-30 20:54:09 +0600

    Pass state and click handler to square component.

--- src/board.tsx
+++ src/board.tsx
@@ -1,29 +1,44 @@
-import React from "react";
+import React, {useState} from "react";
 import {Square} from "./square";
+
+type createHandleClickParams = { boardState: any[]; setBoardState: any, playerState: string, setPlayerState: any }
+type handleClickParams = { squareNumber: number };
+
+function createHandleClick({boardState, setBoardState, playerState, setPlayerState}: createHandleClickParams) {
+    return function ({squareNumber}: handleClickParams) {
+        if (boardState[squareNumber] === null) {
+            setBoardState([...boardState.slice(0, squareNumber), playerState, ...boardState.slice(squareNumber + 1)]);
+            setPlayerState(playerState === 'X' ? 'O' : 'X');
+        }
+
+    }
+}
 
 export function Board() {
     const status = 'Next player: X';
 
-    const boardState = Array(9).fill(null);
+    const [boardState, setBoardState] = useState(Array(9).fill(null));
+    const [playerState, setPlayerState] = useState('X');
+    const handleClick = createHandleClick({boardState, setBoardState, playerState, setPlayerState});
 
     return (
         <div>
             <div className="status">{status}</div>
 
             <div className="board-row">
-                <Square clickedBy={boardState[1]}/>
-                <Square clickedBy={boardState[2]}/>
-                <Square clickedBy={boardState[3]}/>
+                <Square squareNumber={0} clickedBy={boardState[0]} handleClick={handleClick}/>
+                <Square squareNumber={1} clickedBy={boardState[1]} handleClick={handleClick}/>
+                <Square squareNumber={2} clickedBy={boardState[2]} handleClick={handleClick}/>
             </div>
             <div className="board-row">
-                <Square clickedBy={boardState[4]}/>
-                <Square clickedBy={boardState[5]}/>
-                <Square clickedBy={boardState[6]}/>
+                <Square squareNumber={3} clickedBy={boardState[3]} handleClick={handleClick}/>
+                <Square squareNumber={4} clickedBy={boardState[4]} handleClick={handleClick}/>
+                <Square squareNumber={5} clickedBy={boardState[5]} handleClick={handleClick}/>
             </div>
             <div className="board-row">
-                <Square clickedBy={boardState[7]}/>
-                <Square clickedBy={boardState[8]}/>
-                <Square clickedBy={boardState[9]}/>
+                <Square squareNumber={6} clickedBy={boardState[6]} handleClick={handleClick}/>
+                <Square squareNumber={7} clickedBy={boardState[7]} handleClick={handleClick}/>
+                <Square squareNumber={8} clickedBy={boardState[8]} handleClick={handleClick}/>
             </div>
         </div>
     );
--- src/square.tsx
+++ src/square.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 
+type SquareProps = {
+    clickedBy : string;
+    squareNumber: number;
+    handleClick: ({squareNumber}: { squareNumber: number }) => void;
+}
 
-export function Square({clickedBy}: { clickedBy: string }) {
-
-    function onClick() {
-        // setIsClicked(!isClicked);
-    }
-
+export function Square({squareNumber, handleClick, clickedBy}: SquareProps) {
     return (
-        <button className="square" onClick={onClick}>
+        <button className="square" onClick={() => handleClick({squareNumber})}>
             {clickedBy}
         </button>
     );
